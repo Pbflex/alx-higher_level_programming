8d009c0780b9e73b0b9408bdcafbd9a48dbcf1bc
pbflex <bellopeter22291@gmail.com>
2024-05-21 08:21:14 -0700

    Task 8

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
--- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-CREATE table IF NOT EXISTS second_table ('id' INT, 'name' VARCHAR(256), 'SCORE' int);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+-- Write a script that creates a table second_table in the database hbtn_0c_0
+CREATE table IF NOT EXISTS second_table (id INT, name VARCHAR(256), SCORE int);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
