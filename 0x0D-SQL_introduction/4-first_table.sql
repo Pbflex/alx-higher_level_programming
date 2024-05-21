ba99e19301eb4b31124389034e2973d92f874822
pbflex <bellopeter22291@gmail.com>
2024-05-21 07:25:19 -0700

    task

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
--- Write a script that creates a table called first_table in the current database in your MySQL server.
+-- Write a script that creates a table called first_table in the current database.
 CREATE table IF NOT EXISTS first_table('id' INT, 'name' VARCHAR(256));
 
