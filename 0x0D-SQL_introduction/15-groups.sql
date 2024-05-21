7eea6992c571dde9791d58ad6d1b4e56027c8b49
pbflex <bellopeter22291@gmail.com>
2024-05-21 08:54:00 -0700

    Task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,2 @@
---Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+-- Write a script that lists the number of records with the same score
+SELECT score, COUNT(1) AS number FROM second_table GROUP BY score ORDER BY number DESC;
